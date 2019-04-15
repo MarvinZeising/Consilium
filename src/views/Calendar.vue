5ec1f1b1414bde95a0d9cd0a61b613291c19ab87
Marvin Zeising <mzeising@bethel.jw.org>
2019-04-15 11:50:57 +0200

    add Calendar

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,3 +1,71 @@
 <template>
-  <h1>Calendar</h1>
+  <v-layout
+    wrap
+    class="pt-3"
+  >
+
+    <!-- Arrow Left -->
+    <v-flex
+      sm4 xs12
+      class="text-sm-left text-xs-center"
+    >
+      <v-btn @click="$refs.calendar.prev()">
+        <v-icon dark left>keyboard_arrow_left</v-icon>
+        Prev
+      </v-btn>
+    </v-flex>
+
+    <!-- Type chooser -->
+    <v-flex
+      sm4 xs12
+      class="text-xs-center"
+    >
+      <v-select
+        v-model="type"
+        :items="typeOptions"
+        label="Type"
+      />
+    </v-flex>
+
+    <!-- Arrow Right -->
+    <v-flex
+      sm4 xs12
+      class="text-sm-right text-xs-center"
+    >
+      <v-btn @click="$refs.calendar.next()">
+        Next
+        <v-icon right dark>keyboard_arrow_right</v-icon>
+      </v-btn>
+    </v-flex>
+
+    <!-- Calendar -->
+    <v-flex xs12>
+      <v-sheet>
+        <v-calendar
+          ref="calendar"
+          v-model="start"
+          :type="type"
+          :weekdays="weekdays"
+          color="primary"
+        />
+      </v-sheet>
+    </v-flex>
+
+  </v-layout>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import moment from 'moment'
+export default Vue.extend({
+  data: () => ({
+    type: 'month',
+    start: moment().format('yyyy-mm-dd'),
+    weekdays: [1, 2, 3, 4, 5, 6, 0],
+    typeOptions: [
+      { text: 'Week', value: 'week' },
+      { text: 'Month', value: 'month' }
+    ]
+  })
+})
+</script>
