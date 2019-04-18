b3e6ea68473bfa9f212d681cac1dd0818a3de7bf
Marvin Zeising <mzeising@bethel.jw.org>
2019-04-18 13:00:20 +0200

    add dynamic project loading to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -27,6 +27,16 @@
           <v-list-tile-title>Home</v-list-tile-title>
         </v-list-tile>
 
+        <!-- Project loader -->
+        <v-list-tile v-if="loadingProjects">
+          <v-list-tile-action>
+            <v-progress-circular
+              indeterminate
+              color="primary"
+            ></v-progress-circular>
+          </v-list-tile-action>
+          <v-list-tile-title>Loading Projects...</v-list-tile-title>
+        </v-list-tile>
         <!-- Projects -->
         <v-list-group
           v-for="(project, i) in projects"
@@ -108,34 +118,38 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
+
+    <v-snackbar
+      v-model="projectLoadingError"
+      color="error"
+      multi-line="true"
+      timeout=10000
+    >
+      Couldn't reach the server.<br>Are you connected to the internet?
+      <v-btn
+        dark
+        flat
+        @click="projectLoadingError = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
   </nav>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { axiosInstance as axios } from '@/tools/axios'
 export default Vue.extend({
+  created () {
+    this.fetchProjects()
+  },
   data() {
     return {
       drawer: true,
-      projects: [{
-        id: 'asdf',
-        name: 'Wuppertrolley',
-        actions: [
-          ['Knowledge Base', 'subject', '/knowledge-base'],
-          ['Calendar', 'today', '/calendar']
-        ],
-        adminActions: [
-          ['Settings', 'settings', '/settings'],
-          ['User Management', 'group', '/users'],
-          ['Categories', 'label', '/users'],
-          ['Teams', 'supervisor_account', '/users'],
-          ['Meeting Points', 'location_on', '/users'],
-          ['Notifications', 'notifications', '/users'],
-          ['Reports', 'message', '/users'],
-          ['Statistics', 'show_chart', '/users'],
-          ['Notes', 'edit', '/users']
-        ]
-      }],
+      loadingProjects: false,
+      projectLoadingError: false,
+      projects: null,
       profileActions: [
         ['Personal', 'account_circle', '/profile/personal'],
         ['Spiritual', 'assignment_turned_in', '/profile/spritual'],
@@ -144,6 +158,44 @@ export default Vue.extend({
         ['Notifications', 'notifications', '/profile/notifications'],
         ['Configure Projects', 'settings', '/profile/projects']
       ]
+    }
+  },
+  methods: {
+    fetchProjects () {
+      this.loadingProjects = true
+      this.projects = null
+
+      axios.get('/projects')
+        .then((result) => {
+          const fetchedProjects = result.data
+          this.loadingProjects = false
+          this.projects = fetchedProjects.map((project : any) => {
+            return {
+              id: project.id,
+              name: project.text,
+              actions: [
+                ['Knowledge Base', 'subject', '/knowledge-base'],
+                ['Calendar', 'today', '/calendar']
+              ],
+              adminActions: [
+                ['Settings', 'settings', '/settings'],
+                ['User Management', 'group', '/users'],
+                ['Categories', 'label', '/users'],
+                ['Teams', 'supervisor_account', '/users'],
+                ['Meeting Points', 'location_on', '/users'],
+                ['Notifications', 'notifications', '/users'],
+                ['Reports', 'message', '/users'],
+                ['Statistics', 'show_chart', '/users'],
+                ['Notes', 'edit', '/users']
+              ]
+            }
+          })
+        })
+        .catch((err) => {
+          this.loadingProjects = false
+          this.projectLoadingError = true
+          console.error(err.toString())
+        })
     }
   }
 })
