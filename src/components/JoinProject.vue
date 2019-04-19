77675a4ba4c46ad01ecf9f3d56ca2f23e4ad07e2
Marvin Zeising <mzeising@bethel.jw.org>
2019-04-19 18:48:33 +0200

    extract JoinProject component

---
+++ src/components/JoinProject.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="joinProject">
+    <!--//* Join Project -->
+    <v-flex xs12>
+      <h2 class="headline mb-3">Join an existing Project</h2>
+    </v-flex>
+    <v-flex
+      xs12 sm8 md6 lg4
+      class="mb-5"
+    >
+      <p>If you received a Project access token, you can enter the token here to join that Project:</p>
+      <v-text-field
+        v-model="accessToken"
+        label="Project access token"
+      ></v-text-field>
+      <v-btn
+        :disabled="accessToken == ''"
+        color="primary"
+        >
+        Join this Project
+      </v-btn>
+    </v-flex>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data () {
+    return {
+      accessToken: null
+    }
+  }
+})
+</script>
--- src/views/profile/ConfigureProjects.vue
+++ src/views/profile/ConfigureProjects.vue
@@ -5,26 +5,7 @@
 
     <MyProjectInvitations />
 
-    <!--//* Join Project -->
-    <v-flex xs12>
-      <h2 class="headline mb-3">Join an existing Project</h2>
-    </v-flex>
-    <v-flex
-      xs12 sm8 md6 lg4
-      class="mb-5"
-    >
-      <p>If you received a Project access token, you can enter the token here to join that Project:</p>
-      <v-text-field
-        v-model="accessToken"
-        label="Project access token"
-      ></v-text-field>
-      <v-btn
-        :disabled="accessToken == ''"
-        color="primary"
-        >
-        Join this Project
-      </v-btn>
-    </v-flex>
+    <JoinProject />
 
     <!--//* Create Project -->
     <v-flex xs12>
@@ -40,7 +21,6 @@
         Then you can create a new Project and invite all the Publishers to it.
       </p>
       <v-btn
-        v-model="createProject"
         href="create-project"
         color="warning">
         Create a new Project
@@ -54,37 +34,8 @@ import Vue from 'vue'
 import axios from '@/tools/axios'
 import MyProjects from '@/components/MyProjects.vue'
 import MyProjectInvitations from '@/components/MyProjectInvitations.vue'
+import JoinProject from '@/components/JoinProject.vue'
 export default Vue.extend({
-  components: { MyProjects, MyProjectInvitations },
-  data () {
-    return {
-      accessToken: '',
-      createProject: null
-    }
-  },
-  created () {
-    this.fetchInvitations()
-  },
-  methods: {
-    fetchInvitations () {
-      this.loadingInvitations = true
-      this.invitations = []
-
-      axios.get('/projects')
-        .then((result) => {
-          const fetchedInvitations = result.data
-          this.loadingInvitations = false
-          this.invitations = fetchedInvitations.map((project : any) => {
-            return {
-              name: project.text
-            }
-          })
-        })
-        .catch((err) => {
-          this.loadingInvitations = false
-          console.error(err.toString())
-        })
-    }
-  }
+  components: { MyProjects, MyProjectInvitations, JoinProject }
 })
 </script>
