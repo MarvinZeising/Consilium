00aa7b11d93cf88fa0b1b6151becc973d25c2dcf
Marvin Zeising <marvin.zeising@gmail.com>
2019-04-24 19:23:55 +0200

    replace devserver config in vue config

--- Client/vue.config.js
+++ Client/vue.config.js
@@ -1,12 +1,4 @@
 module.exports = {
   lintOnSave: process.env.NODE_ENV !== 'production',
-  runtimeCompiler: true,
-  devServer: {
-    proxy: {
-      '^/api': {
-        target: 'https://localhost:5001',
-        changeOrigin: true
-      }
-    }
-  }
+  runtimeCompiler: true
 }
