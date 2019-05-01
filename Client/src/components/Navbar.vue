0663f3525dddbd80cbc3377e9c5a0a14f34e9923
Marvin Zeising <marvin.zeising@gmail.com>
2019-05-01 11:41:45 +0200

    fix signUp link

--- Client/src/components/Navbar.vue
+++ Client/src/components/Navbar.vue
@@ -97,7 +97,7 @@
         </v-list-group>
 
         <!--//* Sign out -->
-        <v-list-tile to="signUp">
+        <v-list-tile :to="{ name: 'signUp' }">
           <v-list-tile-action>
             <v-icon>exit_to_app</v-icon>
           </v-list-tile-action>
