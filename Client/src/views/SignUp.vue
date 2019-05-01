4d1129045812b18aeaadef00e480b57b4fb24b5e
Marvin Zeising <marvin.zeising@gmail.com>
2019-05-01 20:33:43 +0200

    finish sign up component

--- Client/src/views/SignUp.vue
+++ Client/src/views/SignUp.vue
@@ -22,10 +22,10 @@
 
                 <v-form
                   ref="usernameForm"
-                  v-model="usernameFormValid"
+                  v-model="usernameValid"
                 >
                   <p class="grey--text text--darken-1">
-                    This is the username you will use to login to your Consilium account
+                    This is the username you will use to sign in to your Consilium account
                   </p>
                   <v-text-field
                     v-model="username"
@@ -46,30 +46,45 @@
 
             <v-window-item :value="2">
               <v-card-text>
-                <v-text-field
-                  label="Password"
-                  type="password"
-                ></v-text-field>
-                <v-text-field
-                  label="Confirm Password"
-                  type="password"
-                ></v-text-field>
-                <span class="caption grey--text text--darken-1">
-                  Please enter a password for your account
-                </span>
+
+                <v-form
+                  ref="passwordForm"
+                  v-model="passwordValid"
+                >
+                  <p class="grey--text text--darken-1">
+                    Please choose a password for your account
+                  </p>
+                  <v-text-field
+                    v-model="password"
+                    label="Password"
+                    type="password"
+                    :rules="passwordRules"
+                    box
+                    required
+                  ></v-text-field>
+                  <p class="grey--text text--darken-1">
+                    Just to be sure, please confirm your password one more time
+                  </p>
+                  <v-text-field
+                    v-model="passwordRepeat"
+                    label="Confirm Password"
+                    type="password"
+                    :rules="passwordRepeatRules"
+                    box
+                    required
+                  ></v-text-field>
+                </v-form>
+
               </v-card-text>
             </v-window-item>
 
             <v-window-item :value="3">
               <div class="pa-3 text-xs-center">
-                <v-img
-                  class="mb-3"
-                  contain
-                  height="128"
-                  src="https://cdn.vuetifyjs.com/images/logos/v.svg"
-                ></v-img>
-                <h3 class="title font-weight-light mb-2">Welcome to Vuetify</h3>
-                <span class="caption grey--text">Thanks for signing up!</span>
+                <h2 class="headline">Welcome to Consilium</h2>
+                <p>You successfully created an account.</p>
+                <!--// TODO: add link here -->
+                <p>If you want some tips and tricks on how to get started, you can have a look at our wiki (link here)</p>
+                <p>Otherwise, just go ahead and start planning</p>
               </div>
             </v-window-item>
           </v-window>
@@ -86,12 +101,15 @@
             </v-btn>
             <v-spacer></v-spacer>
             <v-btn
-              :disabled="!usernameFormValid || nextLoading"
+              :disabled="(step === 1 && (!usernameValid || nextLoading)) || (step === 2 && !passwordValid)"
               color="primary"
               @click="next"
             >
-              <span v-if="!nextLoading">
+              <span v-if="!nextLoading && step < 3">
                 Next
+              </span>
+              <span v-if="step === 3">
+                Let's go!
               </span>
               <v-progress-circular
                 v-if="nextLoading"
@@ -118,18 +136,33 @@ export default class SignUp extends Vue {
   private nextLoading: boolean = false
 
   private username: string = ''
-  private usernameFormValid: boolean = false
+  private usernameValid: boolean = false
   private usernameAlreadyExists: boolean = false
   private usernameRules: any[] = [
     (v: string) => !!v || 'Username is required',
     (v: string) => v.length >= 3 || 'Username must be more than 3 characters'
   ]
 
+  private password: string = ''
+  private passwordValid: boolean = false
+  private passwordRules: any[] = [
+    (v: string) => !!v || 'Password is required',
+    (v: string) => v.length >= 8 || 'Password must have at least 8 characters'
+  ]
+
+  private passwordRepeat: string = ''
+  private get passwordRepeatRules() {
+    return [
+      (v: string) => !!v || 'Confirm password is required',
+      (v: string) => v === this.password || 'The passwords have to be equal'
+    ]
+  }
+
   private get currentTitle() {
     switch (this.step) {
       case 1: return 'Sign-up'
       case 2: return 'Create a password'
-      default: return 'Account created'
+      default: return ''
     }
   }
 
@@ -149,6 +182,24 @@ export default class SignUp extends Vue {
               this.usernameAlreadyExists = true
             }
           })
+
+        break
+      }
+      case 2: {
+        const form: any = this.$refs.passwordForm
+
+        if (form.validate()) {
+          await axios.post('/users', {
+            username: this.username,
+            password: this.password
+          })
+          this.step++
+        }
+        break
+      }
+      case 3: {
+        this.$router.push({ name: 'home' })
+        break
       }
     }
   }
