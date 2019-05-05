1fe27179a4ffecd04da8b88a76f72c8e1b935f6f
Marvin Zeising <marvin.zeising@gmail.com>
2019-05-05 21:49:59 +0200

    enable vue devtools in dev environment

--- Client/src/store/index.ts
+++ Client/src/store/index.ts
@@ -4,6 +4,7 @@ import ProjectModule from './modules/projects'
 import UserModule from './modules/users'
 
 Vue.use(Vuex)
+Vue.config.devtools = process.env.NODE_ENV !== 'production'
 
 const store = new Vuex.Store({
   modules: {
