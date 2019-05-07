6364597e27632582094d680c6ab225cad11d5b6b
Boas Lehrke <boas.lehrke@live.de>
2019-05-07 20:51:23 +0200

    add consumation of wiki endpoint

--- Client/src/views/project/KnowledgeBase.vue
+++ Client/src/views/project/KnowledgeBase.vue
@@ -33,51 +33,20 @@ import Vue from 'vue'
 import TabItems from '@/components/TabItems.vue'
 import NewTabDialog from '@/components/dialogs/NewTabDialog.vue'
 import NewTabItemDialog from '@/components/dialogs/NewTabItemDialog.vue'
+import axios from '@/tools/axios'
 export default Vue.extend({
   components: { TabItems, NewTabDialog, NewTabItemDialog },
   data() {
-    return {
-      newTabDialog: null,
+    return { newTabDialog: null,
       newTabItemDialog: null,
       tabsModel: null,
-      tabs: [{
-        name: 'FAQs',
-        items: [{
-          title: 'Et mollit quis duis velit',
-          text: 'Cupidatat dolor ex cupidatat minim do amet ex ullamco.'
-        }]
-      }, {
-        name: 'About the Team',
-        items: [{
-          title: 'Est minim ad cupidatat anim',
-          text: 'Esse irure culpa incididunt cupidatat consequat id.'
-        }]
-      }, {
-        name: 'Meeting-Point Information',
-        items: [{
-          title: 'Non ipsum velit consequat exercitation ut duis',
-          text: 'Ex ex ullamco sint do ullamco ipsum ex ex.'
-        }]
-      }, {
-        name: 'Videos',
-        items: [{
-          title: 'Lorem proident culpa cillum ad',
-          text: 'Aliqua ullamco pariatur proident labore incididunt.'
-        }]
-      }, {
-        name: 'Miscellaneous',
-        items: [{
-          title: 'Laborum irure do exercitation et quis',
-          text: 'Aute velit irure aliqua fugiat ut dolore elit officia in exercitation dolor.'
-        }, {
-          title: 'Lorem non anim proident aliquip in dolore',
-          text: 'Laboris cillum sunt nisi irure commodo sunt exercitation reprehenderit.'
-        }, {
-          title: 'Id nostrud aliquip irure aliquip voluptate',
-          text: 'Cupidatat pariatur laboris do occaecat veniam officia qui sunt laboris dolor amet ut non.'
-        }]
-      }]
-    }
+      tabs: null
+      };
+  },
+  mounted(){
+    axios.get(`/wiki`)
+      .then(response => this.tabs = response.data )
+      .catch( err => this.tabs = null);
   }
 })
 </script>
