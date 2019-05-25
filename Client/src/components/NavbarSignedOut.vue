570455b20d5257200bd6edab4405cf5a9b54f317
Marvin Zeising <marvin.zeising@gmail.com>
2019-05-25 14:35:18 +0200

    client: translate navbarSignOut

--- Client/src/components/NavbarSignedOut.vue
+++ Client/src/components/NavbarSignedOut.vue
@@ -3,12 +3,12 @@
 
     <!--//* Sign in -->
     <v-list-tile :to="{ name: 'signIn' }">
-      <v-list-tile-title>Sign in to your account</v-list-tile-title>
+      <v-list-tile-title>{{ $t('navbar.signIn') }}</v-list-tile-title>
     </v-list-tile>
 
     <!--//* Sign up -->
     <v-list-tile :to="{ name: 'signUp' }">
-      <v-list-tile-title>Create an account</v-list-tile-title>
+      <v-list-tile-title>{{ $t('navbar.signUp') }}</v-list-tile-title>
     </v-list-tile>
 
   </div>
@@ -16,17 +16,10 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { mapGetters, mapActions } from 'vuex'
-import { Project } from '@/models/definitions'
-import { getModule } from 'vuex-module-decorators'
-import ProjectModule from '@/store/modules/projects'
-import UserModule from '@/store/modules/users'
 import Component from 'vue-class-component'
 
 @Component
-export default class Navbar extends Vue {
-
+export default class NavbarSignedOut extends Vue {
   private drawer: boolean = false
-
 }
 </script>
