481637373b91e5ca017369075a874153cb7acd99
Marvin Zeising <marvin.zeising@gmail.com>
2019-05-26 11:13:36 +0200

    client: translate footer

--- Client/src/components/Footer.vue
+++ Client/src/components/Footer.vue
@@ -5,23 +5,26 @@
       :to="{ name: 'termsOfUse' }"
       style="text-decoration:none"
     >
-      Terms of Use
+      {{ $t('navbar.termsOfUse') }}
     </router-link>
     <span class="pl-2 pr-2">|</span>
     <router-link
       :to="{ name: 'privacyPolicy' }"
       style="text-decoration:none"
     >
-      Privacy Policy
+      {{ $t('navbar.privacyPolicy') }}
     </router-link>
     <span class="pl-2 pr-2">|</span>
     <span class="pr-3">
-      0.0.1
+      0.0.1 DEV
     </span>
   </v-footer>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+import Component from 'vue-class-component';
+
+@Component({})
+export default class Footer extends Vue {}
 </script>
