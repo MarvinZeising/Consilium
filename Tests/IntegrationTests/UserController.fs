25f2cb490968b258aeef16c5e892258e04bd2a64
Marvin Zeising <marvin.zeising@gmail.com>
2019-05-27 20:51:57 +0200

    tests: add IntegrationTests folder with tests for email availability endpoint

---
+++ Tests/IntegrationTests/UserController.fs
@@ -0,0 +1,20 @@
+﻿module UserController
+
+open Xunit
+open Shouldly
+open FSharp.Data
+open TestLibrary
+
+[<Fact>]
+let ``GET /users/email-available should return true if available`` () =
+    let result = Http.RequestString <| "http://localhost:5000/users/email-available/" + Randomize.email ()
+
+    result.ShouldBe "true"
+
+[<Fact>]
+let ``GET /users/email-available should return false if not available`` () =
+    let credentials = UserTestDataProvider.createRandomUser
+
+    let result = Http.RequestString <| "http://localhost:5000/users/email-available/" + credentials.email
+
+    result.ShouldBe "false"
---
+++ Tests/IntegrationTests/UserTestDataProvider.fs
@@ -0,0 +1,20 @@
+﻿module UserTestDataProvider
+
+open FSharp.Data
+open FSharp.Data.HttpRequestHeaders
+open Newtonsoft.Json
+open TestLibrary
+open Consilium.UserTypes
+
+let createRandomUser =
+    let credentials = { email = Randomize.email ()
+                        password = Randomize.password () }
+
+    let request = JsonConvert.SerializeObject(credentials);
+
+    Http.Request
+         ( "http://localhost:5000/users",
+           headers = [ ContentType HttpContentTypes.Json ],
+           body = TextRequest request ) |> ignore
+
+    credentials
