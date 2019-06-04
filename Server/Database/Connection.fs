34f4525ff2908c221fa14bfc16fa02b3dea4d52b
Marvin Zeising <marvin.zeising@gmail.com>
2019-06-04 16:12:01 +0200

    server: make db connection env-variable-aware

--- Server/Database/Connection.fs
+++ Server/Database/Connection.fs
@@ -2,14 +2,31 @@ namespace Consilium
 
 module Connection =
 
+    open System
     open MongoDB.Driver
     open UserTypes
     open PersonTypes
     open ProjectTypes
 
-    let private mongo = MongoClient ("mongodb://db:27017/")
-    let private db = mongo.GetDatabase "ConsiliumDb"
+    type Config = {
+        DbUrl: string
+        DbName: string
+        TestServerUrl: string
+    }
 
-    let userCollection = db.GetCollection<User>("users")
-    let personCollection = db.GetCollection<Person>("persons")
-    let projectCollection = db.GetCollection<Project>("projects")
+    let getEnvVar name defaultValue =
+        match Environment.GetEnvironmentVariable name with
+        | null -> defaultValue
+        | value -> value
+
+    let config =
+        { DbUrl = getEnvVar "DB_URL" "mongodb://localhost:27017/"
+          DbName = getEnvVar "DB_NAME" "ConsiliumDb"
+          TestServerUrl = getEnvVar "SERVER_URL" "http://localhost:5000" }
+
+    let private mongo = MongoClient config.DbUrl
+    let private db = mongo.GetDatabase config.DbName
+
+    let userCollection = db.GetCollection<User> "users"
+    let personCollection = db.GetCollection<Person> "persons"
+    let projectCollection = db.GetCollection<Project> "projects"
