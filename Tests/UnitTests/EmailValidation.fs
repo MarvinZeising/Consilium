c5a74644f7f648a0bb6cfdc68aae7fad9d47a37a
Marvin Zeising <marvin.zeising@gmail.com>
2019-12-16 14:49:42 +0100

    tests: fix validate function calls

--- Tests/UnitTests/EmailValidation.fs
+++ Tests/UnitTests/EmailValidation.fs
@@ -3,39 +3,40 @@ module EmailValidation
 open Xunit
 open Consilium
 open CommonTypes
+open CommonValidation
 open TestLibrary
 
 [<Fact>]
 let ``validate should canonicalize email`` () =
-    let result = " tEsT@ExaMplE.COm  " |> EmailValidation.validate
+    let result = " tEsT@ExaMplE.COm  " |> validateEmail
     result |> shouldSucceedWith "test@example.com"
 
 [<Fact>]
 let ``validate email is not empty`` () =
-    let result = "" |> EmailValidation.validate
+    let result = "" |> validateEmail
     result |> shouldContainError EmailRequired
 
 [<Fact>]
 let ``validate email contains at-sign`` () =
-    let result = "wrong" |> EmailValidation.validate
+    let result = "wrong" |> validateEmail
     result |> shouldContainError EmailInvalid
 
 [<Fact>]
 let ``validate email contains at-sign after character`` () =
-    let result = "@wrong" |> EmailValidation.validate
+    let result = "@wrong" |> validateEmail
     result |> shouldContainError EmailInvalid
 
 [<Fact>]
 let ``validate email contains dot`` () =
-    let result = "wrong@address" |> EmailValidation.validate
+    let result = "wrong@address" |> validateEmail
     result |> shouldContainError EmailInvalid
 
 [<Fact>]
 let ``validate email contains dot after characters`` () =
-    let result = "wrong@.com" |> EmailValidation.validate
+    let result = "wrong@.com" |> validateEmail
     result |> shouldContainError EmailInvalid
 
 [<Fact>]
 let ``validate email contains dot before characters`` () =
-    let result = "wrong@address." |> EmailValidation.validate
+    let result = "wrong@address." |> validateEmail
     result |> shouldContainError EmailInvalid
--- Tests/UnitTests/ProjectValidation.fs
+++ Tests/UnitTests/ProjectValidation.fs
@@ -3,20 +3,21 @@
 open Xunit
 open Consilium
 open CommonTypes
+open CommonValidation
 open TestLibrary
 
 [<Fact>]
 let ``validate should return name`` () =
-    let result = "My super Project" |> ProjectValidation.validateName
+    let result = "My super Project" |> validateName
     result |> shouldSucceedWith "My super Project"
 
 [<Fact>]
 let ``validate name is not empty`` () =
-    let result = "" |> ProjectValidation.validateName
+    let result = "" |> validateName
     result |> shouldContainError NameRequired
 
 [<Fact>]
 let ``validate name is not more than 40 characters`` () =
     let result = "waytoomanycharacters.morethan40aretoomuch"
-                 |> ProjectValidation.validateName
+                 |> validateName
     result |> shouldContainError NameLength
