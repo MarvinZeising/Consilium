d195bcf4094b317584b24901c81581236e91216f
Marvin Zeising <marvin.zeising@gmail.com>
2019-12-19 13:21:57 +0100

    client: add no topics display

--- Client/src/components/Topics.vue
+++ Client/src/components/Topics.vue
@@ -1,59 +1,59 @@
 <template>
-  <div>
-    <v-flex xs12>
-      <h2
-        class="headline mb-3"
-        v-t="'knowledgeBase.topics'"
-      />
-    </v-flex>
-    <v-flex
-      xs12 sm10 md8 lg6
-      class="mb-5 pa-2"
+  <v-flex xs12 sm10 md8 lg6>
+    <h2
+      class="headline mb-3"
+      v-t="'knowledgeBase.topics'"
+    />
+    <v-card
+      flat
+      class="ma-2 mb-5"
     >
-      <p>
+      <v-card-text class="grey--text">
         {{ $t('knowledgeBase.topicsDescription') }}
-      </p>
-      <v-card flat>
-        <v-list>
-          <v-list-tile
-            v-for="(topic, index) in getTopics"
-            :key="topic.name"
-          >
-            <v-list-tile-content>
-              <v-list-tile-title v-text="topic.name" />
-            </v-list-tile-content>
+      </v-card-text>
+      <v-list>
+        <v-list-tile
+          class="grey lighten-3 grey--text text--darken-2"
+          v-if="getTopics.length === 0"
+        >
+          <span v-t="'knowledgeBase.noTopics'" />
+        </v-list-tile>
+        <v-list-tile
+          v-for="(topic, index) in getTopics"
+          :key="topic.name"
+        >
+          <v-list-tile-content>
+            <v-list-tile-title v-text="topic.name" />
+          </v-list-tile-content>
 
-            <v-list-tile-action style="flex-direction:row; align-items:center;">
-              <v-btn
-                v-if="index > 0"
-                icon
-                class="ma-0"
-                @click="knowledgeBaseModule.moveTopicUp(topic.order)"
-              >
-                <v-icon color="grey">expand_less</v-icon>
-              </v-btn>
-              <v-btn
-                v-if="index < getTopics.length - 1"
-                icon
-                class="ma-0"
-                @click="knowledgeBaseModule.moveTopicDown(topic.order)"
-              >
-                <v-icon color="grey">expand_more</v-icon>
-              </v-btn>
-              <RenameTopicDialog :topicId="topic.id" />
-              <DeleteTopicDialog :topicId="topic.id" />
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list>
-      </v-card>
-      <v-flex
-        xs12
-        class="mt-2"
-      >
+          <v-list-tile-action style="flex-direction:row; align-items:center;">
+            <v-btn
+              v-if="index > 0"
+              icon
+              class="ma-0"
+              @click="knowledgeBaseModule.moveTopicUp(topic.order)"
+            >
+              <v-icon color="grey">expand_less</v-icon>
+            </v-btn>
+            <v-btn
+              v-if="index < getTopics.length - 1"
+              icon
+              class="ma-0"
+              @click="knowledgeBaseModule.moveTopicDown(topic.order)"
+            >
+              <v-icon color="grey">expand_more</v-icon>
+            </v-btn>
+            <RenameTopicDialog :topicId="topic.id" />
+            <DeleteTopicDialog :topicId="topic.id" />
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+      <v-card-actions>
+        <v-spacer />
         <NewTopicDialog />
-      </v-flex>
-    </v-flex>
-  </div>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
 </template>
 
 <script lang="ts">
