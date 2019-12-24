bfa8ecda49deb9769a50c5266ab065af6a82ec64
Marvin Zeising <marvin.zeising@gmail.com>
2019-12-24 14:29:20 +0100

    client: remove old join and invite components

--- Client/src/views/profile/ConfigureProjects.vue
+++ Client/src/views/profile/ConfigureProjects.vue
@@ -3,47 +3,15 @@
 
     <PersonalProjects />
 
-    <PersonalProjectInvitations />
-
-    <JoinProject />
-
-    <!--//* Create Project -->
-    <v-flex xs12>
-      <h2
-        class="headline mb-3"
-        v-t="'project.create'"
-      />
-    </v-flex>
-    <v-flex
-      xs12 sm10 md8 lg6 xl4
-      class="mb-5"
-    >
-      <p>
-        {{ $t('project.createDescription1') }}
-        <br>
-        {{ $t('project.createDescription2') }}
-      </p>
-      <v-btn
-        :to="{ name: 'createProject' }"
-        color="warning"
-        v-t="'project.create'"
-      />
-    </v-flex>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import PersonalProjects from '../../components/PersonalProjects.vue'
-import PersonalProjectInvitations from '../../components/PersonalProjectInvitations.vue'
-import JoinProject from '../../components/JoinProject.vue'
 
 @Component({
-  components: {
-    PersonalProjects,
-    PersonalProjectInvitations,
-    JoinProject
-  }
+  components: { PersonalProjects }
 })
 export default class ConfigureProjects extends Vue {}
 </script>
