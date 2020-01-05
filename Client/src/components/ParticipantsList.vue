3017eed9151e70f698da7bdea6433f84b243e57b
Marvin Zeising <marvin.zeising@gmail.com>
2020-01-06 00:55:45 +0100

    client: add basic participants list

---
+++ Client/src/components/ParticipantsList.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-flex xs12>
+    <h2 class="headline mb-3">
+      {{ $tc('project.participant.participants', 2) }}
+    </h2>
+    <v-card
+      flat
+      class="ma-2 mb-5"
+    >
+      <v-data-table
+        :headers="headers"
+        :items="items"
+        :items-per-page="15"
+      />
+    </v-card>
+  </v-flex>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Watch } from 'vue-property-decorator'
+import { getModule } from 'vuex-module-decorators'
+import moment from 'moment'
+import UserModule from '../store/users'
+import { Person, ParticipationStatus, Gender } from '../models/definitions'
+
+@Component
+export default class ParticipantsList extends Vue {
+  private userModule: UserModule = getModule(UserModule, this.$store)
+
+  private headers: any[] = [
+    { text: 'First name', value: 'firstname' },
+    { text: 'Last name', value: 'lastname' },
+    { text: 'Role', value: 'role' },
+  ]
+
+  private items: any[] = [
+    {
+      firstname: 'Marvin',
+      lastname: 'Zeising',
+      role: 'Administrator',
+    },
+  ]
+
+}
+</script>
--- Client/src/views/project/Participants.vue
+++ Client/src/views/project/Participants.vue
@@ -6,6 +6,8 @@
 
       <ParticipantsRequests />
 
+      <ParticipantsList />
+
     </v-layout>
   </v-container>
 </template>
@@ -17,11 +19,13 @@ import ProjectModule from '../../store/projects'
 import { Project } from '../../models/definitions'
 import ParticipantsInvitations from '../../components/ParticipantsInvitations.vue'
 import ParticipantsRequests from '../../components/ParticipantsRequests.vue'
+import ParticipantsList from '../../components/ParticipantsList.vue'
 
 @Component({
   components: {
     ParticipantsInvitations,
     ParticipantsRequests,
+    ParticipantsList,
   }
 })
 export default class Participants extends Vue {
