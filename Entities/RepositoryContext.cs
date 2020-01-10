2a0ab64eb12a8ba496a29c28843d9564bab38e03
Marvin Zeising <marvin.zeising@gmail.com>
2020-01-10 18:20:50 +0100

    server: add missing DbSets

--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -12,5 +12,11 @@ namespace Entities
 
         public DbSet<User> Users { get; set; }
         public DbSet<Person> Persons { get; set; }
+        public DbSet<Congregation> Congregations { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Participation> Participations { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Article> Articles { get; set; }
     }
 }
