c8b769f46c589f0dbd91762c98904a1e41cab0ab
Marvin Zeising <marvin.zeising@gmail.com>
2020-01-25 14:30:44 +0100

    client: adapt to new simplified call signatures

--- Client/src/store/applications.ts
+++ Client/src/store/applications.ts
@@ -1,18 +1,16 @@
 import axios from 'axios'
 import { Module, VuexModule, Action } from 'vuex-module-decorators'
 import store from '../plugins/vuex'
-import { Application, Shift } from '../models'
+import { Application, Shift, Person } from '../models'
 
 @Module({ dynamic: true, store, name: 'ApplicationModule' })
 export default class ApplicationModule extends VuexModule {
 
   @Action
   public async createApplication(application: Application) {
-    const { personId, projectId } = this.context.getters.resolvePersonAndProject
+    const personId = this.context.getters.getActivePersonId
 
-    const response = await axios.post(
-      `/persons/${personId}/projects/${projectId}/shifts/${application.shiftId}/applications`,
-      application)
+    const response = await axios.post(`/persons/${personId}/applications`, application)
     const createdApplication = Application.create(response.data)
 
     const shift: Shift = await this.context.dispatch('getShift', application.shiftId)
@@ -23,9 +21,9 @@ export default class ApplicationModule extends VuexModule {
 
   @Action
   public async deleteApplication(application: Application) {
-    const { personId, projectId } = this.context.getters.resolvePersonAndProject
+    const personId = this.context.getters.getActivePersonId
 
-    await axios.delete(`/persons/${personId}/projects/${projectId}/shifts/${application.shiftId}/applications/${application.id}`)
+    await axios.delete(`/persons/${personId}/applications/${application.id}`)
 
     const shift: Shift = await this.context.dispatch('getShift', application.shiftId)
     if (shift) {
