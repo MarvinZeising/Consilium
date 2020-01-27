fb076ee4eb6d833c455b905cdb0081b4ed7711a8
Marvin Zeising <marvin.zeising@gmail.com>
2020-01-27 23:07:26 +0100

    client: add isCaptain toggle to ShiftAssignmentDialog

--- Client/src/components/dialogs/ShiftAssignmentDialog.vue
+++ Client/src/components/dialogs/ShiftAssignmentDialog.vue
@@ -28,9 +28,12 @@
         class="my-4"
         no-gutters
       >
-        <v-col cols="8">
+        <v-col
+          cols="8"
+          style="min-height:100px;"
+        >
 
-          <v-list>
+          <v-list v-if="shift.applications.length > 0">
             <v-list-item
               v-for="(application, applicationIndex) in shift.applications"
               :key="applicationIndex"
@@ -46,7 +49,7 @@
                     class="mt-1 mr-1"
                     v-text="team.name"
                     small
-                    :color="assignments[application.personId] === team.id ? 'accent' : ''"
+                    :color="personIsInTeam(application.personId, team.id) ? 'accent' : ''"
                     @click="toggleTeam(application.personId, team.id)"
                   />
                 </div>
@@ -61,14 +64,23 @@
               </v-list-item-action>
             </v-list-item>
           </v-list>
+          <div
+            v-else
+            class="d-flex align-center justify-center"
+            style="height:100%;"
+          >
+            <div v-text="$tc('shift.application.applicants', 0)" />
+          </div>
 
         </v-col>
         <v-divider vertical />
         <v-col>
 
           <v-list
+            v-if="getHasAttendee"
             class="mx-2"
             dense
+            rounded
           >
             <div
               v-for="(team, teamIndex) in projectModule.getActiveProject.getTeams"
@@ -92,16 +104,71 @@
                   {{ getAttendees(team.id).length }} / 5
                 </v-chip>
               </v-subheader>
-              <v-list-item
+
+              <v-menu
                 v-for="(attendee, attendeeIndex) in getAttendees(team.id)"
                 :key="attendeeIndex"
+                offset-x
               >
-                <v-list-item-content v-if="attendee">
-                  <v-list-item-title v-text="attendee.getFullName" />
-                </v-list-item-content>
-              </v-list-item>
+                <template v-slot:activator="{ on: menu }">
+                  <v-list-item v-on="{ ...menu }">
+                    <v-list-item-content v-if="attendee">
+                      <v-list-item-title>
+                        <u v-if="assignments[attendee.id].isCaptain">{{ attendee.getFullName }}</u>
+                        <span v-else>{{ attendee.getFullName }}</span>
+                      </v-list-item-title>
+                    </v-list-item-content>
+                    <v-list-item-action>
+                      <v-icon small>more_horiz</v-icon>
+                    </v-list-item-action>
+                  </v-list-item>
+                </template>
+                <v-list
+                  v-if="attendee"
+                  dense
+                >
+                  <v-list-item
+                    v-if="assignments[attendee.id].isCaptain"
+                    @click="demote(attendee.id)"
+                  >
+                    <v-list-item-avatar
+                      class="ma-0"
+                      min-width="20px"
+                      width="20px"
+                      height="20px"
+                      left
+                    >
+                      <v-icon small>outlined_flag</v-icon>
+                    </v-list-item-avatar>
+                    <v-list-item-title class="ml-2">Demote from captain</v-list-item-title>
+                  </v-list-item>
+                  <v-list-item
+                    v-else
+                    @click="promote(attendee.id)"
+                  >
+                    <v-list-item-avatar
+                      class="ma-0"
+                      min-width="20px"
+                      width="20px"
+                      height="20px"
+                      left
+                    >
+                      <v-icon small>flag</v-icon>
+                    </v-list-item-avatar>
+                    <v-list-item-title class="ml-2">Promote to captain</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+
             </div>
           </v-list>
+          <div
+            v-else
+            class="d-flex align-center justify-center"
+            style="height:100%;"
+          >
+            <div v-text="$tc('shift.attendee.attendees', 0)" />
+          </div>
 
         </v-col>
       </v-row>
@@ -169,8 +236,18 @@ export default class ShiftAssignmentDialog extends Vue {
   private saving = false
   private releasing = false
 
-  private savedAssignments: any = {}
-  private assignments: any = {}
+  private savedAssignments: {
+    [personId: string]: {
+      teamId: string,
+      isCaptain: boolean
+    }
+  } = {}
+  private assignments: {
+    [personId: string]: {
+      teamId: string,
+      isCaptain: boolean
+    }
+  } = {}
 
   private get canEdit() {
     const role = this.personModule.getActiveRole
@@ -189,7 +266,8 @@ export default class ShiftAssignmentDialog extends Vue {
     }
 
     for (const personId of Object.keys(this.assignments)) {
-      if (this.assignments[personId] !== this.savedAssignments[personId]) {
+      if (this.assignments[personId]?.teamId !== this.savedAssignments[personId]?.teamId
+       || this.assignments[personId]?.isCaptain !== this.savedAssignments[personId]?.isCaptain) {
         return false
       }
     }
@@ -197,30 +275,60 @@ export default class ShiftAssignmentDialog extends Vue {
     return true
   }
 
+  private get getHasAttendee() {
+    for (const personId of Object.keys(this.assignments)) {
+      if (this.assignments[personId] !== undefined) {
+        return true
+      }
+    }
+    return false
+  }
+
   private opened() {
+    Vue.set(this, 'assignments', {})
+    Vue.set(this, 'savedAssignments', {})
+
     if (this.shift?.attendees) {
       for (const attendee of this.shift.attendees) {
-        Vue.set(this.assignments, attendee.personId, attendee.teamId)
+        Vue.set(this.assignments, attendee.personId, {
+          teamId: attendee.teamId,
+          isCaptain: attendee.isCaptain,
+        })
       }
       Object.assign(this.savedAssignments, this.assignments)
     }
   }
 
-  private toggleTeam(personId: string, teamId: string) {
-    if (this.assignments[personId] === teamId) {
-      Vue.set(this.assignments, personId, undefined)
-    } else {
-      Vue.set(this.assignments, personId, teamId)
-    }
-  }
-
   private getAttendees(teamId: string) {
     return Object.keys(this.assignments)
-      .filter((x) => this.assignments[x] === teamId)
+      .filter((x) => this.assignments[x] && this.assignments[x].teamId === teamId)
       .map((x) => this.shift?.applications.find((y) => y.personId === x)?.person)
+      .sort((a, b) => {
+        if (a && b) {
+          if (this.assignments[a.id].isCaptain && !this.assignments[b.id].isCaptain) {
+            return -1
+          } else if (!this.assignments[a.id].isCaptain && this.assignments[b.id].isCaptain) {
+            return 1
+          } else if (a.lastname < b.lastname) {
+            return -1
+          } else if (a.lastname > b.lastname) {
+            return 1
+          } else if (a.firstname < b.firstname) {
+            return -1
+          } else if (a.firstname > b.firstname) {
+            return 1
+          } else {
+            return 0
+          }
+        } else {
+          return 0
+        }
+      })
   }
 
-  private openPerson() {}
+  private personIsInTeam(personId: string, teamId: string) {
+    return this.assignments[personId]?.teamId === teamId
+  }
 
   private async reset() {
     const copy = {}
@@ -237,8 +345,7 @@ export default class ShiftAssignmentDialog extends Vue {
         .map((personId) => {
           return {
             personId,
-            teamId: this.assignments[personId],
-            isCaptain: false,
+            ...this.assignments[personId],
           }
         })
 
@@ -268,6 +375,31 @@ export default class ShiftAssignmentDialog extends Vue {
       this.releasing = false
     }
   }
+
+  private toggleTeam(personId: string, teamId: string) {
+    if (this.assignments[personId]?.teamId === teamId) {
+      Vue.set(this.assignments, personId, undefined)
+    } else {
+      Vue.set(this.assignments, personId, {
+        teamId,
+        isCaptain: false,
+      })
+    }
+  }
+
+  private demote(personId: string) {
+    const item = this.assignments[personId]
+    item.isCaptain = false
+    Vue.set(this.assignments, personId, item)
+  }
+
+  private promote(personId: string) {
+    const item = this.assignments[personId]
+    item.isCaptain = true
+    Vue.set(this.assignments, personId, item)
+  }
+
+  private openPerson() {}
 
   @Emit('saved') private emitSaved() { /* nothing to do */ }
 
