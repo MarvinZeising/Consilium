6baf511a1677a7cbfc38ccbcd66e44c6456a6610
Marvin Zeising <marvin.zeising@gmail.com>
2020-02-23 18:12:18 +0100

    client: add SwitchControl

---
+++ ClientApp/src/components/controls/SwitchControl.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-flex xs12 sm6 md4 class="pa-2">
+    <span v-if="description">{{ $t(description) }}</span>
+
+    <v-switch
+      v-model="model.value"
+      :label="label ? $t(label) : false"
+      color="primary"
+      inset
+      hide-details
+      @change="toggleTeamCaptain"
+    />
+  </v-flex>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Watch, Prop } from 'vue-property-decorator'
+import { getModule } from 'vuex-module-decorators'
+import i18n from '../../i18n'
+
+@Component
+export default class SwitchControl extends Vue {
+  @Prop(Object)
+  private readonly model?: { value: string }
+
+  @Prop(String)
+  private readonly label?: string
+
+  @Prop(String)
+  private readonly description?: string
+}
+</script>
